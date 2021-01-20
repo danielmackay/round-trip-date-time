2f731e8c6d723f9fe6df3de521ab9a0bb2004e53
Daniel Mackay <daniel.mackay@levo.com.au>
2021-01-20 20:32:50 +1000

    Fixed up spacing.

--- Program.cs
+++ Program.cs
@@ -9,16 +9,13 @@ Console.ReadKey();
 
 void PrintRTTFormat()
 {
-    DateTime dat = new DateTime(2009, 6, 15, 13, 45, 30,
-                            DateTimeKind.Unspecified);
+    DateTime dat = new DateTime(2009, 6, 15, 13, 45, 30, DateTimeKind.Unspecified);
     Console.WriteLine("{0} ({1}) --> {0:O}", dat, dat.Kind);
 
-    DateTime uDat = new DateTime(2009, 6, 15, 13, 45, 30,
-                                 DateTimeKind.Utc);
+    DateTime uDat = new DateTime(2009, 6, 15, 13, 45, 30, DateTimeKind.Utc);
     Console.WriteLine("{0} ({1}) --> {0:O}", uDat, uDat.Kind);
 
-    DateTime lDat = new DateTime(2009, 6, 15, 13, 45, 30,
-                                 DateTimeKind.Local);
+    DateTime lDat = new DateTime(2009, 6, 15, 13, 45, 30, DateTimeKind.Local);
     Console.WriteLine("{0} ({1}) --> {0:O}\n", lDat, lDat.Kind);
 
     DateTimeOffset dto = new DateTimeOffset(lDat);
@@ -41,26 +38,24 @@ void PrintRTTConversion()
 	originalDate = DateTime.SpecifyKind(new DateTime(2008, 4, 10, 6, 30, 0), DateTimeKind.Local);
 	dateString = originalDate.ToString("o");
 	newDate = DateTime.Parse(dateString, null, DateTimeStyles.RoundtripKind);
-	Console.WriteLine("Round-tripped {0} {1} to {2} {3}.", originalDate, originalDate.Kind,
-					  newDate, newDate.Kind);
+	Console.WriteLine("Round-tripped {0} {1} to {2} {3}.", originalDate, originalDate.Kind, newDate, newDate.Kind);
 	// Round-trip a UTC time.
 	originalDate = DateTime.SpecifyKind(new DateTime(2008, 4, 12, 9, 30, 0), DateTimeKind.Utc);
 	dateString = originalDate.ToString("o");
 	newDate = DateTime.Parse(dateString, null, DateTimeStyles.RoundtripKind);
-	Console.WriteLine("Round-tripped {0} {1} to {2} {3}.", originalDate, originalDate.Kind,
-					  newDate, newDate.Kind);
+	Console.WriteLine("Round-tripped {0} {1} to {2} {3}.", originalDate, originalDate.Kind, newDate, newDate.Kind);
 	// Round-trip time in an unspecified time zone.
 	originalDate = DateTime.SpecifyKind(new DateTime(2008, 4, 13, 12, 30, 0), DateTimeKind.Unspecified);
 	dateString = originalDate.ToString("o");
 	newDate = DateTime.Parse(dateString, null, DateTimeStyles.RoundtripKind);
-	Console.WriteLine("Round-tripped {0} {1} to {2} {3}.", originalDate, originalDate.Kind,
-					  newDate, newDate.Kind);
+	Console.WriteLine("Round-tripped {0} {1} to {2} {3}.", originalDate, originalDate.Kind, newDate, newDate.Kind);
 
 	// Round-trip a DateTimeOffset value.
 	DateTimeOffset originalDTO = new DateTimeOffset(2008, 4, 12, 9, 30, 0, new TimeSpan(-8, 0, 0));
 	dateString = originalDTO.ToString("o");
 	DateTimeOffset newDTO = DateTimeOffset.Parse(dateString, null, DateTimeStyles.RoundtripKind);
 	Console.WriteLine("Round-tripped {0} to {1}.", originalDTO, newDTO);
+
 	// The example displays the following output:
 	//    Round-tripped 4/10/2008 6:30:00 AM Local to 4/10/2008 6:30:00 AM Local.
 	//    Round-tripped 4/12/2008 9:30:00 AM Utc to 4/12/2008 9:30:00 AM Utc.
